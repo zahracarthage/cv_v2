5ed85033d92fb2a648bf08ec101d6c7fcce2b34c
Zahra carthage <zahracarthage.chouchene@esprit.tn>
2021-10-16 23:05:32 +0100

    Initial commit

---
+++ app/src/main/java/com/example/cv_v2/Activity1.kt
@@ -0,0 +1,71 @@
+package com.example.cv_v2
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.text.TextUtils
+import android.util.Patterns
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cv_v2.databinding.Activity1Binding
+
+
+class Activity1 : AppCompatActivity() {
+
+    private lateinit var binding: Activity1Binding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_1)
+        binding = Activity1Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+    fun isValidEmail(target: CharSequence?): Boolean {
+        return !TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches()
+    }
+
+    fun next(view: android.view.View) {
+
+         if(check())
+       {
+            val intent = Intent(applicationContext,HobbiesActivity::class.java)
+
+
+            startActivity(intent)
+            finish()
+      }
+
+
+
+
+    }
+    private fun check(): Boolean {
+        if(binding.fullName.text.isNullOrEmpty())
+        {
+            Toast.makeText(applicationContext,"please Enter your full name",Toast.LENGTH_SHORT).show()
+            return false
+
+        }
+        if(binding.email.text.isNullOrEmpty() )
+        {
+            Toast.makeText(applicationContext, "Enter your E-mail",Toast.LENGTH_LONG).show()
+            return false
+
+        }
+
+
+        if (!isValidEmail(binding.email.text))
+        {
+            Toast.makeText(getApplicationContext(),"invalid email address",Toast.LENGTH_SHORT).show()
+            return false
+
+        }
+        if(binding.Age.text.isNullOrEmpty())
+        {
+            Toast.makeText(getApplicationContext(),"Invalid Age",Toast.LENGTH_SHORT).show()
+            return false
+        }
+        else return true
+    }
+}
---
+++ app/src/main/java/com/example/cv_v2/HobbiesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cv_v2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HobbiesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hobbies)
+    }
+}
